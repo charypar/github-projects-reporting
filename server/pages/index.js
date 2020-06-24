dc183e01624e33fdbe5bc9e8600fe04d986bbc55
Viktor Charypar <charypar@gmail.com>
2020-06-24 15:19:25 +0100

    Add instructions to the home page

--- server/pages/index.js
+++ server/pages/index.js
@@ -1,5 +1,38 @@
 function HomePage() {
-    return <h1>Export changes to cards on a GitHub project board to CSV! 🤓</h1>
-  }
-  
-  export default HomePage
+  return (
+    <div>
+      <h1>Github Projects Export</h1>
+      <p>Export changes to cards on a GitHub project board to CSV! 🤓</p>
+      <h2>What</h2>
+      <p>
+        This service will give you a CSV export of statistics from a Github
+        Project board. You can use this in a spreadsheet to calculate various
+        delivery statistics, e.g. a Cumulative Flow Diagram. This is meant to be
+        used with Google Sheets' and others' <code>IMPORTDATA</code> function to
+        fetch straight into a spreadsheet.
+      </p>
+      <h2>Getting started</h2>
+      <p>You will need:</p>
+      <ul>
+        <li>A github repository with issues and project boards</li>
+        <li>
+          A{" "}
+          <a href="https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line">
+            personal access token
+          </a>{" "}
+          which can read the issues in the repository (the "repo" permission).
+        </li>
+        <li>A google spreadsheets</li>
+      </ul>
+      <p>
+        For the <code>IMPORTDATA</code> function, you need a URL of the
+        following shape
+      </p>
+      <pre>
+        https://github-projects-reporting.vercel.app/api/[owner]/[repo]?token=[your-token]
+      </pre>
+    </div>
+  );
+}
+
+export default HomePage;
